125608d40dd008ef6ca4eebf8612d557480494e2
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-11-06 23:22:19 +0800

    11.6

---
+++ processpool.h
@@ -0,0 +1,7 @@
+﻿//
+// Created by A qian yang on 2023/11/6.
+//
+#ifndef PROCESSPOOL_H_
+#define PROCESSPOOL_H_
+
+#endif//PROCESSPOOL_H_
--- test.cpp
+++ test.cpp
@@ -1,46 +1,54 @@
-//代码清单14-1
-#include "lock.h"
+//代码清单14-3
+
+#include "processpool.h"
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <wait.h>
 
-int a = 0;
-int b = 0;
-pthread_mutex_t mutex_a;
-pthread_mutex_t mutex_b;
+pthread_mutex_t mutex;
 
+//子进程运行的函数，它首先获得互斥锁mutex，然后暂停5s再释放该互斥锁
 void *another(void *arg)
 {
-    pthread_mutex_lock(&mutex_b);
-    printf("in child thread, got mutex b, waiting for mutex a\n");
+    printf("in child thread, lock the mutex\n");
+    pthread_mutex_lock(&mutex);
     sleep(5);
-    ++b;
-    pthread_mutex_lock(&mutex_a);
-    b += a++;
-    pthread_mutex_unlock(&mutex_a);
-    pthread_mutex_unlock(&mutex_b);
-    pthread_exit(NULL);
+    pthread_mutex_unlock(&mutex);
 }
 
 int main()
 {
+    pthread_mutex_init(&mutex, NULL);
     pthread_t id;
-
-    pthread_mutex_init(&mutex_a, NULL);
-    pthread_mutex_init(&mutex_b, NULL);
     pthread_create(&id, NULL, another, NULL);
+    //父进程中的主线程暂停1s，以确保在执行fork操作之前，子线程已经开始运行并获得了互斥变量mutex
+    sleep(1);
+    int pid = fork();
+    if (pid < 0)
+    {
+        pthread_join(id, NULL);
+        pthread_mutex_destroy(&mutex);
+        return 1;
+    }
+    else if (pid == 0)
+    {
+        printf("I am in the child, want to get the lock\n");
+        //子进程从父进程继承了互斥锁mutex的状态，该互斥锁处于锁住的状态，这是由父进程中的子线程执行pthread_mutex_lock引起的，因此下面这句话加锁操作会一直阻塞，尽管从逻辑上来说它是不应该阻塞的
 
-    pthread_mutex_lock(&mutex_a);
-    printf("in parent thread, got mutex a, waiting for mutex b\n");
-    sleep(5);
-    ++a;
-    pthread_mutex_lock(&mutex_b);
-    a += b++;
-    pthread_mutex_unlock(&mutex_b);
-    pthread_mutex_unlock(&mutex_a);
-
+        //这句话注释掉，就可以正常运行了，不会重复加锁，
+        pthread_mutex_lock(&mutex);
+        printf("I can not run to here, oop...\n");
+        pthread_mutex_unlock(&mutex);
+        exit(0);
+    }
+    else
+    {
+        wait(NULL);
+    }
     pthread_join(id, NULL);
-    pthread_mutex_destroy(&mutex_a);
-    pthread_mutex_destroy(&mutex_b);
+    pthread_mutex_destroy(&mutex);
+
     return 0;
 }
