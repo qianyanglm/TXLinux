629c3e06efa6660cb17ccc5c023ddf66e60b1b8f
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-11-17 13:37:49 +0800

    11.17

--- test.c
+++ test.c
@@ -2,6 +2,18 @@
 
 int main()
 {
-    printf("%lu", sizeof("http://"));
+    int a[5] = {1, 2, 3, 4, 5};
+    int *ptr = (int *) (&a + 1);
+    printf("%d %d\n", *(a + 1), *(ptr - 1));
+
+    printf("a = %p\n", a);
+    printf("&a = %p\n", &a);
+    printf("&a[0] = %p\n", &a[0]);
+    printf("&a[4] = %p\n", &a[4]);
+    printf("ptr = %p\n", ptr);
+    printf("&a+1 = %p\n", &a + 1);
+    printf("*ptr = %d\n", *ptr);
+    printf("ptr-1 = %p\n", ptr - 1);
+
     return 0;
 }
